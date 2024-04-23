7782f8d0be96c37e0104b9057d21107020e78d19
Mykyta Opanasiuk <nikitaopanassiuk@outlook.com>
2024-04-24 01:04:53 +0300

    Website optimization and accessibility improvement

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,20 @@ const sass = require("gulp-sass")(require("sass"));
 const browsersync = require("browser-sync").create();
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const htmlmin = require("gulp-htmlmin");
 
 function sassTask() {
   return src("./assets/styles/scss/style.scss")
     .pipe(sass())
-    .pipe(postcss([autoprefixer()]))
+    .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(dest("./assets/styles/css/"));
+}
+
+function htmlTask() {
+  return src("./html/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest("./"));
 }
 
 function browsersyncServe(cb) {
@@ -30,4 +38,4 @@ function watchTask() {
   watch("./assets/styles/**/*.scss", series(sassTask, browsersyncReload));
 }
 
-exports.default = series(sassTask, browsersyncServe, watchTask);
+exports.default = series(htmlTask, sassTask, browsersyncServe, watchTask);
